f0ee442c73326d84565f6b8d75e0d82ad9847d88
Darina Pohribniak <dpogribnyak@gmail.com>
2022-01-30 20:20:42 +0200

    added files Module2HW1

---
+++ Mod2HW1/Actions.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.IO;
+
+namespace Mod2HW1
+{
+    public enum EventType
+    {
+        Info,
+        Warning,
+        Error,
+    }
+
+    public class Actions
+    {
+        private readonly Logger _logger = Logger.MyInstance;
+        private readonly Result _result = new Result();
+
+        public Result FirstMetod()
+        {
+            _logger.AddLog(EventType.Info, "Start method: FirstMetod");
+            _result.Status = true;
+            _result.Message = " ";
+            return _result;
+        }
+
+        public Result SecondMetod()
+        {
+            _logger.AddLog(EventType.Warning, "Skipped logic in method: SecondMetod");
+            _result.Status = true;
+            _result.Message = " ";
+            return _result;
+        }
+
+        public Result ThirdMetod()
+        {
+            _logger.AddLog(EventType.Error, " ");
+            _result.Status = false;
+            _result.Message = "I broke a logic";
+            return _result;
+        }
+
+        public void ExportToFile()
+        {
+           File.WriteAllText("log.txt", _logger.LogData);
+        }
+
+        public void LogError(string errorMes)
+        {
+            _logger.AddLog(EventType.Error, $"Action failed by a reason: {errorMes}");
+        }
+    }
+}
---
+++ Mod2HW1/Result.cs
@@ -0,0 +1,45 @@
+﻿namespace Mod2HW1
+{
+    public class Result
+    {
+        private bool _status;
+        private string _message;
+
+        public Result()
+        {
+        }
+
+        public bool Status
+        {
+            get
+            {
+                return _status;
+            }
+
+            set
+            {
+                if (value)
+                {
+                    _status = true;
+                }
+                else
+                {
+                    _status = false;
+                }
+            }
+        }
+
+        public string Message
+        {
+            get
+            {
+                return _message;
+            }
+
+            set
+            {
+                _message = "I broke a logic";
+            }
+        }
+    }
+}
