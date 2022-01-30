1c76beb1334045a6610490be5b862f90c69d8c78
Darina Pohribniak <dpogribnyak@gmail.com>
2022-01-30 19:02:11 +0200

    added files from Module2HW1

---
+++ Mod2HW1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Mod2HW1
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            Starter starter = new Starter();
+            starter.Run();
+            Console.ReadLine();
+        }
+    }
+}
